87bb7a96c5a84fc38c5ae37dac82f95345b1ae49
asuravk <Ankit.Suravkar@morningstar.com>
2022-08-13 00:43:28 +0530

    profile pages and avatars added

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from email.policy import default
+from hashlib import md5
 from sqlite3 import Timestamp
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +13,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,6 +28,10 @@ class User(db.Model, UserMixin):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
