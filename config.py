efec69610e7f8f8522d5b31fe141b92f8af55c15
asuravk <Ankit.Suravkar@morningstar.com>
2023-01-21 13:12:41 +0530

    saving the changes

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER='smtp.googlemail.com'
-    MAIL_PORT=587
-    MAIL_USE_TLS=1
-    MAIL_USERNAME='ajava2848@gmail.com'
-    MAIL_PASSWORD=''
-    ADMINS = ['ajava2848@gmail.com']
-    POSTS_PER_PAGE = 10
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
+    POSTS_PER_PAGE = 25
